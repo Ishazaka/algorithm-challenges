3d64876491a75cba102f539570f57f583e6a9fa3
Isha Zaka <90307713+Ishazaka@users.noreply.github.com>
2022-01-14 05:29:05 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 
+
+
+// code1 (a)
+const markmass = 78;
+const markheight = 1.69;
+const johnmass = 95;
+const johnheight = 1.95;
+
+// formula here
+const bMImark = markmass / (markheight * markheight);
+ const bMIjohn = johnmass /( johnheight **2);
+ console.log(bMImark,bMIjohn);
+
+ const highermarkbmi = bMImark > bMIjohn;
+ console.log( highermarkbmi);
