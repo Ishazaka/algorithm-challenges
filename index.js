f6a21d034d725da44f69fcdd664e80ac9ae62ec8
Isha Zaka <90307713+Ishazaka@users.noreply.github.com>
2022-01-18 18:48:35 +0500

    Update index.js

--- index.js
+++ index.js
@@ -156,3 +156,8 @@ checkthewinner(scoresdolphin, scoreskoalas)
 // const calctip = function(bill){
 //  return  bill <= 300 && bill >=50 ? bill * 0.15 :bill* 0.20;
 // }
+
+const calctips = bill => bill <= 300 && bill >= 50? bill* 0.15 : bill*0.2;
+
+ const bill = [125,555, 44] ;
+ const tips = [calctips(bill[0]), calctips(bill[1]), calctips(bill[2])];
