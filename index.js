d3b99c64cd78aed7cc386809e1fc5a18afab3802
Isha Zaka <90307713+Ishazaka@users.noreply.github.com>
2022-01-18 18:48:58 +0500

    Update index.js

--- index.js
+++ index.js
@@ -161,3 +161,8 @@ const calctips = bill => bill <= 300 && bill >= 50? bill* 0.15 : bill*0.2;
 
  const bill = [125,555, 44] ;
  const tips = [calctips(bill[0]), calctips(bill[1]), calctips(bill[2])];
+console.log(bill, tips);
+
+ const total = [bill[0] + tips[0],bill[1] + tips[1],bill[2] + tips[2] ];
+
+ console.log(total);
